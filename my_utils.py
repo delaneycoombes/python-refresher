04ee693217a140998e92d2ec970977a92f4c2f2e
Delaney Coombes <deco5379@colorado.edu>
2026-08-27 19:54:52 +0000

    added line parsing

--- my_utils.py
+++ my_utils.py
@@ -1,2 +1,11 @@
 def get_column(file_name, query_column, query_value, result_column):
-    return None
+    results = []
+
+    with open(file_name, "r") as file:
+        for line in file:
+            values = line.split()
+
+            if values[query_column] == query_value:
+                results.append(values[result_column])
+                
+    return results

b6fe2c0022e2d2a3143cb75cd95cffd9484f0d5b
Delaney Coombes <deco5379@colorado.edu>
2026-09-03 21:44:14 +0000

    Add default column

--- print_fires.py
+++ print_fires.py
@@ -1,9 +1,9 @@
 import my_utils
 
 country='United States of America'
-county_column = 0
+country_column = 0
 fires_column = 3
 file_name = 'Agrofood_co2_emission.csv'
-fires = my_utils.get_column(file_name, country_column, country, fires_column)
+fires = my_utils.get_column(file_name, country_column, country, result_column = fires_column)
 
 print(fires)

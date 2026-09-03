8f5a783c75b806954fdf30fc5634f9c9d433c288
Delaney Coombes <deco5379@colorado.edu>
2026-09-03 21:43:47 +0000

    fix fxn to return results

--- my_utils.py
+++ my_utils.py
@@ -1,9 +1,9 @@
-def get_column(file_name, query_column, query_value, result_column):
+def get_column(file_name, query_column, query_value, result_column = 1):
    
     # create an empty array to store results
     results = []
 
-    with open(file_name, "r") as file:
+    with open(file_name, 'r') as file:
         for line in file:
             values = line.split(',')
 
@@ -11,4 +11,4 @@ def get_column(file_name, query_column, query_value, result_column):
             if values[query_column] == query_value:
                 results.append(values[result_column])
                 
-    return None
+    return results

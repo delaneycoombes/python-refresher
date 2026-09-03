8226b1c3094f691bbefed60fb3f6a802b3c67626
Delaney Coombes <deco5379@colorado.edu>
2026-09-03 21:26:51 +0000

    Update get_column function

--- my_utils.py
+++ my_utils.py
@@ -1,2 +1,13 @@
 def get_column(file_name, query_column, query_value, result_column):
+   
+    # create an empty list to store results
+    results = []
+
+    with open(file_name, "r") as file:
+        for line in file:
+            values = line.split(',')
+
+            if values[query_column] == query_value:
+                results.append(values[result_column])
+                
     return None

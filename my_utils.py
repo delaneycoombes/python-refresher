05b4ef05bab44b03e3359ee899fa12e99240b605
Delaney Coombes <deco5379@colorado.edu>
2026-09-03 21:27:34 +0000

    Update get_column function

--- my_utils.py
+++ my_utils.py
@@ -1,12 +1,13 @@
 def get_column(file_name, query_column, query_value, result_column):
    
-    # create an empty list to store results
+    # create an empty array to store results
     results = []
 
     with open(file_name, "r") as file:
         for line in file:
             values = line.split(',')
 
+            # check if the column contains desired country
             if values[query_column] == query_value:
                 results.append(values[result_column])
                 

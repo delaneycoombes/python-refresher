9df4aacb16a9617766c9ac260a5965f2574fea72
Delaney Coombes <deco5379@colorado.edu>
2026-09-17 07:53:38 +0000

    fix pycodestyle errors

--- my_utils.py
+++ my_utils.py
@@ -1,14 +1,21 @@
-def get_column(file_name, query_column, query_value, result_column = 1):
-   
+def get_column(file_name, query_column, query_value, result_column=1):
+
     # create an empty array to store results
     results = []
 
-    with open(file_name, 'r') as file:
-        for line in file:
-            values = line.split(',')
+    try:
+        with open(file_name, 'r') as file:
+            for line in file:
+                values = line.split(',')
 
-            # check if the column contains desired country
-            if values[query_column] == query_value:
-                results.append(values[result_column])
-                
+                # check if the column contains desired country
+                if values[query_column] == query_value:
+                    try:
+                        results.append(int(round(
+                            float(values[result_column]))))
+                    except ValueError:
+                        pass
+    except OSError:
+        print('Error: file could not be opened.')
+
     return results

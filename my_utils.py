626e872319a8ada9193fde9e1fe287294d19ef24
Delaney Coombes <deco5379@colorado.edu>
2026-09-24 04:29:50 +0000

    task 1 to add mean, median, and std dev functions

--- my_utils.py
+++ my_utils.py
@@ -19,3 +19,43 @@ def get_column(file_name, query_column, query_value, result_column=1):
         print('Error: file could not be opened.')
 
     return results
+
+
+def mean(arr):
+    # display error message for empty arrays
+    if not arr:
+        raise ValueError("The mean of an empty array cannot be computed")
+
+    # calculate the mean of an array of integers
+    return sum(arr) / len(arr)
+
+def median(arr):
+    # display error message for empty arrays
+    if not arr:
+        raise ValueError("The median of an empty array cannot be computed")
+
+    # sort and count the array elements
+    sort = sorted(arr)
+    n = len(sort)
+    
+    # divide by two and round down
+    mid = n / / 2
+
+    # calculate the median of an array of integers
+    if n % 2 == 0:
+        return(sort[mid - 1] + sort[mid]) / 2
+    return float(sort[mid])
+
+def standard_deviation(arr):
+    # display error message for empty arrays
+    if not arr:
+        raise ValueError("The standard deviation of an empty array cannot be computed")
+
+    # calculate the mean and variance
+    m = mean(arr)
+    variance = sum((x - m) ** 2 for x in arr) / len(arr)
+
+    # calculate standard devication which is the square root of the variance
+    return variance ** 0.5
+
+    
